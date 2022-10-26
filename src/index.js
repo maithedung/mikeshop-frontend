6e8e63517a861c547aa700e7914c461485728a0d
maithedung <44361757+maithedung@users.noreply.github.com>
2022-10-26 08:37:51 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import App from "./App";
 import {Provider} from "react-redux";
 import store from "./Redux/store";
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
-    document.getElementById("root")
-);
+ReactDOM.render(<Provider store={store}>
+    <App/>
+</Provider>, document.getElementById("root"));
