5e1af5ba693e5b36496e1ded0fd78442f0a9227c
dungmt8 <44361757+maithedung@users.noreply.github.com>
2022-12-11 20:15:57 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import {Provider} from "react-redux";
 import store from "./Redux/store";
+import {Widget} from "./widget/Widget";
 
 ReactDOM.render(<Provider store={store}>
     <App/>
+    <Widget/>
 </Provider>, document.getElementById("root"));
