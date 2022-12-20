94cb86aa955b394c2a0adb7104ba3d3f1fbb6531
dungmt8 <44361757+maithedung@users.noreply.github.com>
2022-12-20 16:11:01 +0700

    update

---
+++ src/Redux/Actions/Auth/AuthOtpValidateActions.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import {URL} from "../../Url";
+import {logout} from "../User/UserLogoutActions";
+import {
+    AUTH_OTP_VALIDATE_FAIL, AUTH_OTP_VALIDATE_REQUEST, AUTH_OTP_VALIDATE_SUCCESS
+} from "../../Constants/Auth/AuthOtpValidateConstants";
+import {USER_NOT_AUTHORIZED_ERROR} from "../../Messages";
+
+export const validateAuthOtp = (userId, token) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: AUTH_OTP_VALIDATE_REQUEST
+        })
+        const {
+            userLogin: {userInfo}
+        } = getState()
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json", "Authorization": `Bearer ${userInfo.token}`
+            }
+        }
+
+        const authData = {userId: userId, token: token}
+
+        const {data} = await axios.post(`${URL}/api/auth/otp/validate`, authData, config)
+        dispatch({
+            type: AUTH_OTP_VALIDATE_SUCCESS, payload: data
+        })
+    } catch (error) {
+        const message = error.response && error.response.data.message ? error.response.data.message : error.message
+        if (message === USER_NOT_AUTHORIZED_ERROR) {
+            dispatch(logout())
+        }
+        dispatch({
+            type: AUTH_OTP_VALIDATE_FAIL, payload: message
+        })
+    }
+}
