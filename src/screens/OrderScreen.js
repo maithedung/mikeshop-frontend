f723617b76938da98199e50ca66ccb09c8c05443
dungmt8 <44361757+maithedung@users.noreply.github.com>
2022-12-21 19:17:00 +0700

    Update OrderScreen.js

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -10,7 +10,7 @@ import moment from "moment";
 import axios from "axios";
 import {ORDER_PAY_RESET} from "../Redux/Constants/Order/OrderPayConstants";
 import {payOrder} from "../Redux/Actions/Order/OrderPayActions";
-import {URL} from "../Redux/Url"
+import {HOST} from "../Redux/Url"
 
 const OrderScreen = ({match}) => {
     window.scrollTo(0, 0);
@@ -36,7 +36,7 @@ const OrderScreen = ({match}) => {
 
     useEffect(() => {
         const addPayPayScript = async () => {
-            const {data: clientId} = await axios.get(`${URL}/api/config/paypal`)
+            const {data: clientId} = await axios.get(`${HOST}/api/config/paypal`)
             const script = document.createElement("script")
             script.type = "text/javascript"
             script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
