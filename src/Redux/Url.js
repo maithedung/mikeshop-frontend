9388467b383c7796ed6e400a4f58d84e58fe78ff
dungmt8 <44361757+maithedung@users.noreply.github.com>
2022-12-21 19:16:57 +0700

    Update Url.js

--- src/Redux/Url.js
+++ src/Redux/Url.js
@@ -1,1 +1,10 @@
-export const URL = process.env.REACT_APP_SERVER_URL
+export const HOST = process.env.REACT_APP_SERVER_URL
+export const AUTH_OTP_DISABLE_URL = `${HOST}/api/auth/otp/disable`
+export const AUTH_OTP_GENERATE_URL = `${HOST}/api/auth/otp/generate`
+export const AUTH_OTP_VALIDATE_URL = `${HOST}/api/auth/otp/validate`
+export const AUTH_OTP_VERIFY_URL = `${HOST}/api/auth/otp/verify`
+export const ORDER_URL = `${HOST}/api/orders`
+export const PRODUCT_URL = `${HOST}/api/products`
+export const USER_PROFILE_URL = `${HOST}/api/users/profile`
+export const LOGIN_URL = `${HOST}/api/users/login`
+export const REGISTER_URL = `${HOST}/api/users/register`
