7d532b268b8bb6471f017d0b444c40b645383cd0
dungmt8 <44361757+maithedung@users.noreply.github.com>
2022-12-27 07:19:41 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import { render } from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
-// import {Widget} from "./widget/Widget";
+import {Widget} from "./widget/Widget";
 
 render(
   <StrictMode>
     <Provider store={store}>
       <App />
-      {/*<Widget/>*/}
+      <Widget/>
     </Provider>
   </StrictMode>,
   document.getElementById("root")
