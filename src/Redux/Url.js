637a278172210b42fe44bb39d5099212e1a67350
dungmt8 <44361757+maithedung@users.noreply.github.com>
2022-12-27 07:04:15 +0700

    update

--- src/Redux/Url.js
+++ src/Redux/Url.js
@@ -1,4 +1,5 @@
 export const HOST = process.env.REACT_APP_SERVER_URL
+export const MIKE_CHAT = "https://mikechat.onrender.com";
 export const AUTH_OTP_DISABLE_URL = `${HOST}/api/auth/otp/disable`
 export const AUTH_OTP_GENERATE_URL = `${HOST}/api/auth/otp/generate`
 export const AUTH_OTP_VALIDATE_URL = `${HOST}/api/auth/otp/validate`
